f2032943dedc7078d5978ae4374494e28187f693
Студент Студентов <is@athene.tech>
2025-12-16 10:30:17 +0300

    Ветка D

--- ConflictDemo/main.cpp
+++ ConflictDemo/main.cpp
@@ -7,7 +7,7 @@ int main() {
 	std::cout << "Новое изменение X" << std::endl;
 
 	cout << "This is FEATURE 2" << endl; //+2 izmenenie
-	cout << "This is FEATURE 1" << endl; //+1 izmenenie
+	cout << "This is FEATURE 133" << endl; //+1 izmenenie
 	return 0;
 
 }
