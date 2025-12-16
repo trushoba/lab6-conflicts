83006548621ec2fb2f45ccc40c39b5dfac0c91eb
Студент Студентов <is@athene.tech>
2025-12-16 10:29:28 +0300

    Ветка C

--- ConflictDemo/main.cpp
+++ ConflictDemo/main.cpp
@@ -3,11 +3,9 @@ using namespace std;
 int main() {
 
 	std::cout << "Новое изменение Y" << std::endl;
-
 	std::cout << "Новое изменение X" << std::endl;
-
 	cout << "This is FEATURE 2" << endl; //+2 izmenenie
-	cout << "This is FEATURE 1" << endl; //+1 izmenenie
+	cout << "This is FEATURE 12" << endl; //+1 izmenenie 
 	return 0;
 
 }
